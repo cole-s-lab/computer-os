619ad282686fe21ccb7f133a387df4f2bc275bca
Cole Neville <5910739+ColeNeville@users.noreply.github.com>
2025-06-26 19:02:22 -0600

    build: create /usr/local/lib for global pip installs

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,6 +9,8 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+mkdir -p /usr/local/lib # Required for global pip installs
+
 # Developer tools
 dnf5 install -y \
   gcc \
